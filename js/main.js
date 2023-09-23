d7a77eb835483bbbf2a43e62ef868a0107e1562e
Ayoub RABIYA <rabiyaeayoub@gmail.com>
2023-09-23 15:58:09 +0100

    Fix some errors and new features

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+const inptsContainer = document.querySelector('.date-inputs');
 const dayInp = document.querySelector('#day');
 const monthInp = document.querySelector('#month');
 const yearInp = document.querySelector('#year');
-const submitBtn = document.querySelector('.submit-btn button');
 const currentDate = new Date();
 const yearsNumberOut = document.querySelector('#years .number');
 const yearsTextOut = document.querySelector('#years .text');
@@ -9,6 +9,24 @@ const monthsNumberOut = document.querySelector('#months .number');
 const monthsTextOut = document.querySelector('#months .text');
 const daysNumberOut = document.querySelector('#days .number');
 const daysTextOut = document.querySelector('#days .text');
+const form = document.querySelector("#form");
+
+const calculateDate = (day, month, year) => {
+  let daysResult = currentDate.getDate() - day;
+  let monthsResult = (currentDate.getMonth() + 1) - month;
+  let yearsResult = currentDate.getFullYear() - year;
+  
+  if (daysResult < 0) {
+    daysResult = 30 + daysResult;
+    monthsResult -= 1;
+  }
+  if (monthsResult < 0) {
+    monthsResult = 12 + monthsResult;
+    yearsResult -= 1;
+  }
+  
+  return { daysResult, monthsResult, yearsResult };
+};
 
 const showAge = (age) => {
   const years = age.yearsResult;
@@ -19,34 +37,44 @@ const showAge = (age) => {
   if (months === 1) monthsTextOut.textContent = 'month';
   if (days === 1) daysTextOut.textContent = 'day';
 
-  yearsNumberOut.textContent = years;
-  monthsNumberOut.textContent = months;
-  daysNumberOut.textContent = days;
-};
 
-const calculateDate = (day, month, year) => {
-  let daysResult = currentDate.getDate() - day;
-  let monthsResult = (currentDate.getMonth() + 1) - month;
-  let yearsResult = currentDate.getFullYear() - year;
+  let y = 0;
+  const yearsint = setInterval(() => {
+    yearsNumberOut.textContent = y++;
+    if(y > years) {
+      clearInterval(yearsint);
+    }
+  }, 100);
+  let m = 0;
+  const monthsint = setInterval(() => {
+    monthsNumberOut.textContent = m++;
+    if(m > months) {
+      clearInterval(monthsint);
+    }
+  }, 100);
+  let d = 0;
+  const daysint = setInterval(() => {
+    daysNumberOut.textContent = d++;
+    if(d > days) {
+      clearInterval(daysint);
+    }
+  }, 100);
 
-  if (daysResult < 0) {
-    daysResult = 30 + daysResult;
-    monthsResult -= 1;
-  }
-  if (monthsResult < 0) {
-    monthsResult = 12 + monthsResult;
-    yearsResult -= 1;
-  }
-
-  return { daysResult, monthsResult, yearsResult };
+  // Remove Errors
+  inptsContainer.classList.remove('error-date-inputs');
+  [dayInp, monthInp, yearInp].forEach(input => input.nextElementSibling.textContent = "");
 };
 
 const showAnError = (inp, msg) => {
   const input = inp;
-  const inptsContainer = document.querySelector('.date-inputs');
   inptsContainer.classList.add('error-date-inputs');
 
   input.nextElementSibling.textContent = msg;
+
+  
+  yearsNumberOut.textContent = "--";
+  monthsNumberOut.textContent = "--";
+  daysNumberOut.textContent = "--";
 };
 
 const dateValidate = (dayInput, monthInput, yearInput) => {
@@ -67,7 +95,7 @@ const dateValidate = (dayInput, monthInput, yearInput) => {
   const month = Number(monthInput);
   const year = Number(yearInput);
 
-  if (currentDate.getFullYear() > year && year > 0) {
+  if (currentDate.getFullYear() > year && year >= 1900) {
     if (month > 12 || month < 1) {
       showAnError(monthInp, 'Must be a valid month');
     } else if (month === 4) {
@@ -113,6 +141,7 @@ const dateValidate = (dayInput, monthInput, yearInput) => {
   }
 };
 
-submitBtn.addEventListener('click', () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   dateValidate(dayInp.value, monthInp.value, yearInp.value);
 });
